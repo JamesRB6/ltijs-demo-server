85bc1bc5e4cd0cceba13a389953d168a3b51e9cc
JamesRB6 <162858218+JamesRB6@users.noreply.github.com>
2024-11-05 10:05:46 +1100

    ConfiguredForMoodle

--- index.js
+++ index.js
@@ -1,51 +1,80 @@
 require('dotenv').config()
 const path = require('path')
-const routes = require('./src/routes')
 
+// Require Provider 
 const lti = require('ltijs').Provider
-
-// Setup
-lti.setup(process.env.LTI_KEY,
-  {
-    url: 'mongodb://' + process.env.DB_HOST + '/' + process.env.DB_NAME + '?authSource=admin',
-    connection: { user: process.env.DB_USER, pass: process.env.DB_PASS }
-  }, {
-    staticPath: path.join(__dirname, './public'), // Path to static files
+const Database = require('ltijs-sequelize')
+const routes = require('./src/routes')
+// Setup ltijs-sequelize using the same arguments as Sequelize's generic contructor
+const db = new Database('pfel', 'pfel_owner', 'k7oePX5JCmuQ', 
+  { 
+    host: 'ep-fancy-bar-a7i3x4oe.ap-southeast-2.aws.neon.tech',
+    dialect: 'postgres',
+    logging: false,
+    dialectOptions: {
+      ssl: {
+        require: true,              // Enforce SSL connection
+        rejectUnauthorized: false   // Disable strict SSL certificate validation
+      }
+    }
+  }
+);
+// Setup provider
+lti.setup(process.env.LTI_KEY, // Key used to sign cookies and tokens
+  { 
+    plugin: db // Passing db object to plugin field
+  },
+  { // Options
+    appRoute: '/', loginRoute: '/login', // Optionally, specify some of the reserved routes
     cookies: {
       secure: false, // Set secure to true if the testing platform is in a different domain and https is being used
-      sameSite: '' // Set sameSite to 'None' if the testing platform is in a different domain and https is being used
+      sameSite: 'None' // Set sameSite to 'None' if the testing platform is in a different domain and https is being used
     },
     devMode: true // Set DevMode to true if the testing platform is in a different domain and https is not being used
-  })
+  }
+)
 
-// When receiving successful LTI launch redirects to app
+// Set LTI launch callback
 lti.onConnect(async (token, req, res) => {
-  return res.sendFile(path.join(__dirname, './public/index.html'))
-})
+  console.log("Connected");
 
-// When receiving deep linking request redirects to deep screen
-lti.onDeepLinking(async (token, req, res) => {
-  return lti.redirect(res, '/deeplink', { newResource: true })
-})
+  // Extract query parameters from the original request
+  const query = req.query;
 
-// Setting up routes
+  // Construct the new URL with query parameters
+  const queryString = new URLSearchParams(query).toString();
+  const newUrl = `http://localhost:3050${
+    queryString ? `?${queryString}` : ""
+  }`;
+
+  // Redirect to the new URL
+  res.redirect(newUrl);
+
+  // return res.sendFile(path.join(__dirname, "./public/index.html"));
+});
+
+
+// also use custom routes
 lti.app.use(routes)
 
-// Setup function
 const setup = async () => {
-  await lti.deploy({ port: process.env.PORT })
+  // Deploy server and open connection to the database
+  await lti.deploy({ port: 3000 }) // Specifying port. Defaults to 3000
 
-  /**
-   * Register platform
-   */
-  /* await lti.registerPlatform({
-    url: 'http://localhost/moodle',
-    name: 'Platform',
-    clientId: 'CLIENTID',
-    authenticationEndpoint: 'http://localhost/moodle/mod/lti/auth.php',
-    accesstokenEndpoint: 'http://localhost/moodle/mod/lti/token.php',
-    authConfig: { method: 'JWK_SET', key: 'http://localhost/moodle/mod/lti/certs.php' }
-  }) */
+  const basePlatformUrl = 'https://pathwayforexceptionallearners.moodlecloud.com'; // Base URL
+
+  // Register platform
+  await lti.registerPlatform({
+    url: `https://pathwayforexceptionallearners.moodlecloud.com`, // Base URL
+    name: 'PathwayForExceptionalLearners2', // Platform Name
+    clientId: 'KEc3AxBT8oSb4To', // Client ID
+    authenticationEndpoint: `${basePlatformUrl}/mod/lti/auth.php`, // Full auth endpoint
+    accesstokenEndpoint: `${basePlatformUrl}/mod/lti/token.php`, // Full access token endpoint
+    authConfig: { 
+      method: 'JWK_SET', 
+      key: `https://pathwayforexceptionallearners.moodlecloud.com/mod/lti/certs.php` // Full keyset endpoint
+    }
+  });
 }
 
 setup()
